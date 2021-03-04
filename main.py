3129fa7a30af7aab5e5a03f8e4953f4b6ebf3004
varunsaxena1332001 <varun.saxena_cs18@gla.ac.in>
2021-03-04 21:42:54 +0530

    replacing the full name of IPL teams with short forms of the teams.

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ import pandas as pd
 
 matches = pd.read_csv("IPL Matches 2008-2020.csv")
 deliveries = pd.read_csv("IPL Ball-by-Ball 2008-2020.csv")
+
+team_names = matches.team1.unique()
+print(team_names)
+
+team_names_abv = ["RCB", "KXIP", "DD", "MI", "KKR", "RR", "DeCh", "CSK", "KTK", "PWI", "SH", "GL", "RPS", "RPS", "DC"]
+matches.replace(team_names, team_names_abv, inplace=True)
+
+print(matches)
